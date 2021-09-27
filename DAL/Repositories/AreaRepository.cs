b6c3f3db5dd4514e3a83cb3e6ea39533e233e6b5
phongtrollx2 <tranbinhphongcmg@gmail.com>
2021-09-27 22:48:38 +0700

    initial Data Access Layer (DAL)

---
+++ DAL/Repositories/AreaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev69Restaurant.DAL.Repositories
+{
+    class AreaRepository
+    {
+    }
+}
